3be2f369535c8880f697b087cf083faad8353a35
cray1 <cray1@email.arizona.edu>
2013-10-17 22:24:39 -0700

    chunkFile.sh added ability to chunk up to 99 pieces.

--- chunkFile.sh
+++ chunkFile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sh scriptname filename
-split -dl$((`wc -l $1|sed 's/ .*$//'` / 3 + 1)) $1 $1-chunk.
+# sh scriptname filename numberOfChunks(limit:99)
+split -dl$((`wc -l $1|sed 's/ .*$//'` / $2 + 1)) $1 $1-chunk.
