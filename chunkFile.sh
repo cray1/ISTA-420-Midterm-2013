129123a894de1b2e647a2cecf800070e33ec0c02
cray1 <cray1@email.arizona.edu>
2013-10-24 12:48:01 -0700

    fancied up chunkfile.sh

--- chunkFile.sh
+++ chunkFile.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-# sh scriptname filename numberOfLines
-split -dl$2 $1 segment
+# Run this shell script by typing the following into bash: sh chunkFile.sh filename numberOfLines
+
+#if the directory "splitFiles" does not exist
+if [ ! -d "splitFiles$2" ]; then
+  #make the directory "splitFiles"
+  mkdir splitFiles$2
+fi #end if
+
+#split the file into parts and put in directory splitFiles.
+split -a 6 -d -l $2 $1 splitFiles$2/$1.part_  #note, limited to 999,999 files. To changes this limit edit the number following the -a
