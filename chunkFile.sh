51f146384fa404a68f0276f1a6a475e7da5dd795
cray1 <cray1@email.arizona.edu>
2013-10-24 10:23:47 -0700

    changed chunkFile.sh to split files by number of lines rather than number of files

--- chunkFile.sh
+++ chunkFile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sh scriptname filename numberOfChunks(limit:99)
-split -dl$((`wc -l $1|sed 's/ .*$//'` / $2 + 1)) $1 $1-chunk.
+# sh scriptname filename numberOfLines
+split -dl$2 $1 segment
