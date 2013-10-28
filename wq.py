ca725720e8cd84aa50bf36821584bc88bc21067b
Te Dasinger <dasinger@email.arizona.edu>
2013-10-27 21:20:41 -0700

    Updated wq.py
    Made some changes but only for the tempSplit directory, need help
    changing it to look for specific file numbers.

--- wq.py
+++ wq.py
@@ -4,26 +4,33 @@ from work_queue import *
 import sys
 
 try:
-    Q = WorkQueue(port = 0)
+    Q = WorkQueue(port = 9014)
+    Q.specify_name("Pamplemousse")
 except:
     print "could not instantiate Work Queue master"
     sys.exit(1)
 
 print "Listening on port %d." % Q.port
 
+density = os.listdir('/tempSplit')
+print "splitFiles"
+
 print "Submitting 542 simulation tasks..."
-for i in range(0, 542):
-    infile = "input.txt"
-    outfile = "file.%0.2x" % i
-    command = "python simulation.py %d < %s > %s" % (i, infile, outfile)
-
-    T = Task(command)
-
-    T.specify_file("simulation.py", "simulation.py", WORK_QUEUE_INPUT, cache = True)
-    T.specify_file(infile, infile, WORK_QUEUE_INPUT, cache = False)
-    T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = False)
-
-    taskid = Q.submit(T)
+for i in range(len(density)):
+	print density
+	
+	infile1 = "test1_density_grid.txt.part_00000" + str(i)
+	infile2 = "test1_grav_pos.txt"
+	outfile = "test1_density_grid.txt.part_00000" + str(i) + ".out"
+	command = "python grav.py %s %s > %s" % (infile1, infile2, outfile)
+	
+	T = Task(command)
+	T.specify_file("grav.py", "grav.py", WORK_QUEUE_INPUT, cache = True)
+	T.specify_file(infile1, infile1, WORK_QUEUE_INPUT, cache = False)
+	T.specify_file(infile2, infile2, WORK_QUEUE_INPUT, cache = False)
+	T.specify_file(outfile, outfile, WORK_QUEUE_OUTPUT, cache = False)
+	
+	taskid = Q.submit(T)
 print "done."
 
 print "Waiting for tasks to complete..."
