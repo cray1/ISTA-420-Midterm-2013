ee62fb332d67586cccc95c9bfb292d84faf26e05
cray1 <cray1@email.arizona.edu>
2013-10-29 23:02:54 -0700

    g

--- generateMakeFlow.py
+++ generateMakeFlow.py
@@ -10,6 +10,7 @@ print("    ./sh chunkFile.sh test1_density_grid.txt 100000")
 for i in range(543):
     partNum = '%06d' % i
     fileName = ("test1_density_grid.txt.part_%s" %(partNum))
-    print(fileName+(".out: %s grav.py" %(fileName)))
-    print(("grav.py test1_density_grid.txt.part_%s %s") % (partNum,"test1_grav_pos.txt"))
-    print(("	./python grav.py %s %s") % (fileName,"test1_grav_pos.txt"))
+    outFile = fileName+".out"
+    print(fileName+": grav.py %s" %(fileName))
+    #print(("grav.py test1_density_grid.txt.part_%s %s") % (partNum,"test1_grav_pos.txt"))
+    print(("    python grav.py < %s test1_grav_pos.txt > %s") % (fileName,outFile))
