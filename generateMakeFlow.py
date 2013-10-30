bd14ee44db283ae486ef89f187421bdfc8c07438
cray1 <cray1@email.arizona.edu>
2013-10-29 22:20:22 -0700

    better makeflow

--- generateMakeFlow.py
+++ generateMakeFlow.py
@@ -1,7 +1,15 @@
-print("test1_density_grid.txt test1_grav_pos.txt chunkFile.sh joinFile.sh prism.py grav.py:")
-print("test1_density_grid.txt chunkFile.sh")
-print("    sh chunkFile.sh test1_density_grid.txt 100000")
+outString = ""
 for i in range(543):
     partNum = '%06d' % i
+    outString += (("test1_density_grid.txt.part_%s ")%(partNum))
+outString +=' : test1_density_grid.txt chunkFile.sh'
+print(outString)
+print("    ./sh chunkFile.sh test1_density_grid.txt 100000")
+
+
+for i in range(543):
+    partNum = '%06d' % i
+    fileName = ("test1_density_grid.txt.part_%s" %(partNum))
+    print(fileName+(".out: %s grav.py" %(fileName)))
     print(("grav.py test1_density_grid.txt.part_%s %s") % (partNum,"test1_grav_pos.txt"))
-    print(("	python grav.py test1_density_grid.txt.part_%s %s") % (partNum,"test1_grav_pos.txt"))
+    print(("	./python grav.py %s %s") % (fileName,"test1_grav_pos.txt"))
