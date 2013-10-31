eb2e256e01936091e364051a4af63dbc3488cd2d
cray1 <cray1@email.arizona.edu>
2013-10-31 08:17:43 -0700

    added back in sh....turns out you need to use it when commands are launched in a subprocess

--- generateMakeFlow.py
+++ generateMakeFlow.py
@@ -3,13 +3,13 @@ for i in range(543): #number of files +1
     partNum = '%06d' % i
     outString += ("test1_density_grid.txt.part_%s " %(partNum))
 print(outString+": chunkFile.sh test1_density_grid.txt")
-print("     (chunkFile.sh test1_density_grid.txt 100000)")
+print("     (sh chunkFile.sh test1_density_grid.txt 100000)")
 for i in range(543): #number of files +1
     partNum = '%06d' % i
     fileName = ("test1_density_grid.txt.part_%s" %(partNum))
     outFile = fileName+".out"
     print(outFile+": grav.py %s processWorker.sh" %(fileName))
-    print(("    (processWorker.sh %s)") % (fileName))
+    print(("    (sh processWorker.sh %s)") % (fileName))
 
 #outString = "test1_density_grid.txt.out: " 
 #for i in range(543): #number of files +1
