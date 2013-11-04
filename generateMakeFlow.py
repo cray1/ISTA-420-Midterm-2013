4d2bd10d4a72d5c1853f61e289da61f1ce44e4f0
cray1 <cray1@email.arizona.edu>
2013-11-04 05:29:02 -0700

    generates Makeflow file for FINALLY WORKING MAKEFLOW
    This renders joinFile.sh obsolete. Folders are no longer used.

--- generateMakeFlow.py
+++ generateMakeFlow.py
@@ -1,26 +1,29 @@
-outString = ""
+#generate the makeflow script for partitioning the density grid file. 
+#File is assumed to be called test1_density_grid.txt
+outString=""
 for i in range(543): #number of files +1
     partNum = '%06d' % i
     outString += ("test1_density_grid.txt.part_%s " %(partNum))
-print(outString+": chunkFile.sh test1_density_grid.txt")
-print("     (sh chunkFile.sh test1_density_grid.txt 100000)")
+print(outString+": test1_density_grid.txt chunkFile.sh")
+print("	source chunkFile.sh test1_density_grid.txt 100000")
+
+#generate the makeflow script for running each new partition through grav.py using test1_grav_pos.txt
 for i in range(543): #number of files +1
     partNum = '%06d' % i
     fileName = ("test1_density_grid.txt.part_%s" %(partNum))
     outFile = fileName+".out"
-    print(outFile+": grav.py %s processWorker.sh" %(fileName))
-    print(("    (sh processWorker.sh %s)") % (fileName))
+    print(outFile+" : %s grav.py prism.py test1_grav_pos.txt" %(fileName))
+    print(("    python grav.py %s test1_grav_pos.txt") % (fileName))
 
-#outString = "test1_density_grid.txt.out: " 
-#for i in range(543): #number of files +1
-#    partNum = '%06d' % i
-#    outString += ("outFiles/test1_density_grid.txt.part_%s.out " %(partNum))
-#print(outString)
-#outString="cat"
-#for i in range(543): #number of files +1
-#    partNum = '%06d' % i
-#    outString += (" outFiles/test1_density_grid.txt.part_%s.out" %(partNum))
-#print("    "+outString + " > test1_density_grid.txt.out")
-
-    
-    
+#generate the makeflow script for concatenating all the output files into one final file.
+#final output file will be called test1_density_grid.txt.out
+outString = "test1_density_grid.txt.out : " 
+for i in range(543): #number of files +1
+    partNum = '%06d' % i
+    outString += ("test1_density_grid.txt.part_%s.out " %(partNum))
+print(outString)
+outString="cat"
+for i in range(543): #number of files +1
+    partNum = '%06d' % i
+    outString += (" test1_density_grid.txt.part_%s.out" %(partNum))
+print("    "+outString + " > test1_density_grid.txt.out") 
