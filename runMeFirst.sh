c1e33281b29c38661890a9344490a81992199add
cray1 <cray1@email.arizona.edu>
2013-11-04 05:31:45 -0700

    Update runMeFirst.sh

--- runMeFirst.sh
+++ runMeFirst.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export PATH=~/cctools/bin:${PATH}
 export PYTHONPATH=${PYTHONPATH}:~/cctools/lib/python2.4/site-packages
-(python generateMakeFlow.py > mf.t)
-(mv mf.t Makeflow)
+python generateMakeFlow.py > mf.t
+mv mf.t Makeflow
 torque_submit_workers -a -N PamplemousseFullCalc 542 -c 0
 makeflow -T wq -a -N PamplemousseFullCalc
+
