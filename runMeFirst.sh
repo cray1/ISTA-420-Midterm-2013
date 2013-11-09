81f3a2d883d0c85f03247a55cd7880194927d1ac
cray1 <cray1@email.arizona.edu>
2013-11-09 15:23:11 -0700

    Update runMeFirst.sh

--- runMeFirst.sh
+++ runMeFirst.sh
@@ -4,5 +4,4 @@ export PYTHONPATH=${PYTHONPATH}:~/cctools/lib/python2.4/site-packages
 python generateMakeFlow.py > mf.t
 mv mf.t Makeflow
 torque_submit_workers -a -N PamplemousseFullCalc 542 -c 0
-makeflow -T wq -a -N PamplemousseFullCalc
-
+makeflow -T wq -a -N PamplemousseFullCalc --email="cray1@email.arizona.edu"
